9bcb72f43d2216531284a3a585b7182a90d92216
Nicholas Zachary <nicholas.zachary@outlook.com>
2018-12-05 20:33:42 +0000

    wip - cleanup

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -9,8 +11,17 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  var randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15).substring(0,6);
+  var randomSixCharString = randomString.substring(0,6);
+  console.log("howdy");
+  console.log(randomSixCharString, 'RANDOM STRING');
+  return randomSixCharString;
+}
+
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Helloszz!");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -28,14 +39,45 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  var formLongSubmission = req.body.longURL;
+  var shortUrl           = generateRandomString();
+  urlDatabase[shortUrl] = formLongSubmission;
+  console.log(urlDatabase, 'Url Database');
+  console.log(req.body);  // debug statement to see POST parameters
+  // res.send("Submission received");         // Respond with 'Ok' (we will replace this)
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  generateRandomString();
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-  console.log("ONE BELOW");
-  console.log(req.params.id)
   let templateVars = {
    shortURL: req.params.id,
    longURL: urlDatabase[req.params.id]
  };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log(req.params.shortURL);
+  let shortURL = req.params.shortURL
+  let longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  console.log('POST DELETE REQUEST');
+  console.log(req.params.id)
+  // Delete a resource from the database
+  let shortUrlKey = req.params.id;
+  delete urlDatabase[shortUrlKey];
+  res.redirect("/urls");
+
 });
 
 app.listen(PORT, () => {
